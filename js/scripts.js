ea6c0c602460055af19f4c952547684f7161b99c
dorcaslitunya <anonolitunya@gmail.com>
2018-02-23 14:20:50 +0300

    add borders into page

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,11 @@
-var userSentence = prompt ("Enter a Sentence:");
+$(document).ready(function)(){
+  $("#open-menu").click(function() {
+    $("aside").animate({
+      "left"
+
+    })
+  }
 
 
-//First function
-var outputOne;
 
-function capitalizeLetters() {
- outputOne =userSentence.charAt(0).toUpperCase() + userSentence.charAt(userSentence.length -1).toUpperCase();
 }
-
-//Second function
-function reverseLetters() {
- return outputOne.split("").reverse().join("")
-}
-
-//Third function
- function callBoth(){
-  capitalizeLetters();
-}
-console.log(callBoth());
